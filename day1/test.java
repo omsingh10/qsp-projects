335ab34d4cf4396cd48b96ff74f59d85cea0d29b
omsingh10 <os0106897@gmail.com>
2026-01-05 21:07:56 +0530

    day-1

---
+++ day1/test.java
@@ -0,0 +1,11 @@
+class Test {
+    int a = 10;  
+    public static void main(String[] args) {
+
+        Test obj = new Test();
+        
+        System.out.println("Value of a: " + obj.a);
+
+       
+    }
+}
